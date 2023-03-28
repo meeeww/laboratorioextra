a901c1d72cf1259b05cdf6581ef8c49d0be52209
DESKTOP-C9HHN7V\A8-PC00 <licencias@svalero.com>
2023-03-28 18:46:47 +0200

    Terminado HTML y CSS, empieza JS

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("hola omar")
+
+const precios = [
+    {
+        titulo: "Free",
+        descripcion: "Find aute inure dolor in reprehenderit in volatek",
+        divisa: "$",
+        precio: 0,
+        features: {
+            uno: "Up to 3 projects for each member",
+            dos: "2 team members allowed"
+        }
+    },
+    {
+        titulo: "Standard",
+        descripcion: "Find aute inure dolor in reprehenderit in volatek",
+        divisa: "$",
+        precio: 15,
+        features: {
+            uno: "Unlimited number of projects",
+            dos: "Up to 5 team members",
+            tres: "24/7 support",
+            cuatro: "Up to 30GB storage space"
+        }
+    }
+]
+
+console.log(precios[0])
